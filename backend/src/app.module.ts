ffd0d02bc5f0f96581570c0812b7086c40d6aec5
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-02-20 17:07:20 +0900

    first push

---
+++ backend/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { PostsModule } from './components/posts/posts.module';
+import * as path from 'path';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: path.join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+    PostsModule,
+  ],
+})
+export class AppModule {}
