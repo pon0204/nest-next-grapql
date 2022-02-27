1268cda0a4a1b79b3e6cbe5b6707e652e7155e70
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-02-27 19:34:07 +0900

    feat: カラム追加 ＆ query絞り込み実装

--- backend/src/components/posts/post.resolvers.ts
+++ backend/src/components/posts/post.resolvers.ts
@@ -1,15 +1,15 @@
-import { ConfigService } from '@nestjs/config';
+import { PrismaService } from './../prisma/prisma.service';
 import { Args, Query, Resolver } from '@nestjs/graphql';
-import { PrismaService } from '@pb-components/prisma/prisma.service';
-import { PbEnv } from 'src/config/environments/pb-env.service';
-import { PostModel } from './interfaces/post.model';
+import { PostModel } from '@pb-components/posts/interfaces/post.model';
+import { GetPostsArgs } from './interfaces/get-posts-connection.arg';
 
 @Resolver((of) => PostModel)
 export class PostsResolver {
   constructor(private readonly prisma: PrismaService) {}
 
-  @Query(() => [PostModel], { name: 'posts', nullable: true })
-  async getPosts() {
+  // 役目を終えたのでリネーム
+  @Query(() => [PostModel], { name: 'fixedPosts', nullable: true })
+  async getPostsByFixedData() {
     return [
       {
         id: '1',
@@ -25,5 +25,22 @@ export class PostsResolver {
   @Query(() => [PostModel], { name: 'prismaPosts', nullable: true })
   async getPostsByPrisma() {
     return this.prisma.post.findMany();
+  }
+
+  @Query(() => [PostModel], { name: 'posts', nullable: true })
+  async getPosts(@Args() args: GetPostsArgs) {
+    return this.prisma.post.findMany({
+      where: {
+        type: args.type
+          ? {
+              in: args.type,
+            }
+          : undefined,
+        published: true,
+      },
+      orderBy: {
+        publishDate: 'desc',
+      },
+    });
   }
 }
