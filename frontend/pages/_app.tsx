1bf5b24252eced2765168bba2a6312be7c234943
pon0204 <tsuyoshi.gonzaresu.0204@gmail.com>
2022-03-15 22:37:34 +0900

    feat: mui ＆ emotion導入(エラー有り)

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,30 @@
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import createEmotionCache from '../styles/createEmotionCache'
 import '../styles/globals.css'
+import { theme } from '../styles/theme'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+type PbAppProps = AppProps & {
+  emotionCache?: EmotionCache
+}
+
+function MyApp({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}: PbAppProps) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
 
 export default MyApp
